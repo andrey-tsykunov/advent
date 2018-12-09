3c551acaae4c6f99319dfce9eb2a1d3b81769fcb
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2018-12-08 19:36:02 -0500

    Day 8

---
+++ src/test/scala/advent2018/Day8Test.scala
@@ -0,0 +1,37 @@
+package advent2018
+
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.io.Source
+
+class Day8Test extends FunSuite with Matchers {
+
+  import Day8._
+
+  test("task 1") {
+    val data = readFrom("day8.txt")
+
+    parse(data).sum shouldBe 47464
+  }
+
+  test("task 1 (example)") {
+    val data = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+
+    parse(data).sum shouldBe 138
+  }
+
+  test("task 2") {
+    val data = readFrom("day8.txt")
+
+    parse(data).value shouldBe 23054
+  }
+
+  test("task 2 (example)") {
+    val data = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+
+    parse(data).value shouldBe 66
+  }
+
+  private def readFrom(name: String): String =
+    Source.fromResource(s"advent2018/$name").mkString
+}
