21a0441456df643b78bf84a6a6143ec9ad3a1365
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2018-12-09 22:49:09 -0500

    Day 9

---
+++ src/main/scala/advent2018/Day9.scala
@@ -0,0 +1,54 @@
+package advent2018
+
+object Day9 {
+
+  class RingItem private(val value: Int, var previous: RingItem, var next: RingItem) {
+    def move(count: Int): RingItem = count match {
+      case x if x > 0 => next.move(count - 1)
+      case x if x < 0 => previous.move(count + 1)
+      case 0 => this
+    }
+
+    def remove(): Int = {
+      previous.next = next
+      next.previous = previous
+      value
+    }
+
+    def insert(value: Int): RingItem = {
+      val m = new RingItem(value, this, next)
+
+      this.next.previous = m
+      this.next = m
+      m
+    }
+  }
+
+  object RingItem {
+    def starter() = {
+      val r = new RingItem(0, null, null)
+      r.previous = r
+      r.next = r
+      r
+    }
+  }
+
+  def runGame(playersCount: Int, marblesCount: Int): Long = {
+
+    var current = RingItem.starter()
+    val playersScore = Array.ofDim[Long](playersCount)
+
+    (1 to marblesCount).foreach {
+      case m if m % 23 == 0 =>
+        current = current.move(-7)
+
+        playersScore(m % playersCount) += m + current.remove()
+        current = current.next
+
+      case m =>
+        current = current.move(1).insert(m)
+    }
+
+    playersScore.max
+  }
+}
