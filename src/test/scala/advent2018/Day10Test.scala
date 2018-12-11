5c6b3bf33b3fcb4cf027d6ad892c8ce5faaf11a3
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2018-12-10 21:03:28 -0500

    Day 10

---
+++ src/test/scala/advent2018/Day10Test.scala
@@ -0,0 +1,37 @@
+package advent2018
+
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.io.Source
+
+class Day10Test extends FunSuite with Matchers {
+
+  import Day10._
+
+  test("task 1") {
+    val data = readFrom("day10.txt")
+
+    val (r, sec) = printMinArea(data)
+
+    sec shouldBe 10418
+    println
+    r.foreach(println)
+  }
+
+  test("task 1 (example)") {
+    val data = readFrom("day10_1.txt")
+
+    val (r, sec) = printMinArea(data)
+
+    sec shouldBe 3
+    println
+    r.foreach(println)
+  }
+
+  private def readFrom(name: String): Seq[Point] =
+    Source.fromResource(s"advent2018/$name")
+      .getLines()
+      .map("""[-\d]+""".r.findAllIn(_).map(_.toInt).toList)
+      .map { case l :: t :: vl :: vt :: Nil => Point(l, t, vl, vt) }
+      .toStream
+}
