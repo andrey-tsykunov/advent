cf2dada48d2b0412462cb6c9cc545c3cda34cf77
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2018-12-12 22:50:22 -0500

    Day 11, enabled custom size, added toPar

--- src/main/scala/advent2018/Day11.scala
+++ src/main/scala/advent2018/Day11.scala
@@ -2,36 +2,40 @@ package advent2018
 
 object Day11 {
 
-  def findMaxSquare(serial: Int, maxSize: Int = 300): (Int, Int, Int) = {
+  def findMaxSquare(serial: Int, size: Int = 300, minSize: Int = 1, maxSize: Int = 300): (Int, Int, Int) = {
 
-    val grid = Array.ofDim[Int](301, 301)
+    require(maxSize <= size)
 
-    for(x <- 1 to 300; y <- 1 to 300)
+    val grid = Array.ofDim[Int](size + 1, size + 1)
+
+    for(x <- 1 to size; y <- 1 to size)
       grid(y)(x) = cellPower(x, y, serial)
 
     var maxXyz = (0, 0, 0)
     var max = Int.MinValue
 
     var dp_1 = grid.clone()
-    var dp_2 = Array.ofDim[Int](301, 301)
+    var dp_2 = Array.ofDim[Int](size + 1, size + 1)
 
     for (z <- 2 to maxSize) {
 
-      val next = Array.ofDim[Int](301, 301)
+      val next = Array.ofDim[Int](size + 1, size + 1)
 
-      for (x <- 1 to 301 - z; y <- 1 to 301 - z) {
-        val bottomLeft = dp_1(y + 1)(x)
-        val topRight = dp_1(y)(x + 1)
-        val center = dp_2(y + 1)(x + 1)
+      (1 to size + 1 - z).toParArray.foreach { x =>
+        for (y <- 1 to size + 1 - z) {
+          val bottomLeft = dp_1(y + 1)(x)
+          val topRight = dp_1(y)(x + 1)
+          val center = dp_2(y + 1)(x + 1)
 
-        val power = bottomLeft + topRight - center + grid(y)(x) + grid(y + z - 1)(x + z - 1)
+          val power = bottomLeft + topRight - center + grid(y)(x) + grid(y + z - 1)(x + z - 1)
 
-        if(power > max) {
-          max = power
-          maxXyz = (x, y, z)
+          if(power > max && z >= minSize) {
+            max = power
+            maxXyz = (x, y, z)
+          }
+
+          next(y)(x) = power
         }
-
-        next(y)(x) = power
       }
 
       dp_2 = dp_1
--- src/test/scala/advent2018/Day11Test.scala
+++ src/test/scala/advent2018/Day11Test.scala
@@ -9,11 +9,15 @@ class Day11Test extends FunSuite with Matchers {
   import Day11._
 
   test("task 1") {
-    findMaxSquare(1133, 3) shouldBe (235,14,3)
+    findMaxSquare(1133, maxSize = 3) shouldBe (235,14,3)
   }
 
   test("task 2") {
     findMaxSquare(1133) shouldBe (237,227,14)
+  }
+
+  test("task 3") {
+    findMaxSquare(1133, size = 1000, maxSize = 1000) shouldBe (477,295,19)
   }
 
   test("cell power") {
@@ -26,8 +30,8 @@ class Day11Test extends FunSuite with Matchers {
 
   test("findMaxSquare 3x3") {
 
-    findMaxSquare(18, 3) shouldBe (33, 45, 3)
-    findMaxSquare(42, 3) shouldBe (21, 61, 3)
+    findMaxSquare(18, minSize = 3, maxSize = 3) shouldBe (33, 45, 3)
+    findMaxSquare(42, minSize = 3, maxSize = 3) shouldBe (21, 61, 3)
   }
 
   test("findMaxSquare any size") {
