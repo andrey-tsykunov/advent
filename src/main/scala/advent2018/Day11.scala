94a9845669644406f7e4475766028ff8b24d74cb
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2018-12-12 22:37:05 -0500

    Day 11, optimized to have O(N^2) memory

--- src/main/scala/advent2018/Day11.scala
+++ src/main/scala/advent2018/Day11.scala
@@ -3,29 +3,39 @@ package advent2018
 object Day11 {
 
   def findMaxSquare(serial: Int, maxSize: Int = 300): (Int, Int, Int) = {
-    val dp = Array.ofDim[Int](301, 301, 301)
+
+    val grid = Array.ofDim[Int](301, 301)
 
     for(x <- 1 to 300; y <- 1 to 300)
-      dp(y)(x)(1) = cellPower(x, y, serial)
+      grid(y)(x) = cellPower(x, y, serial)
 
     var maxXyz = (0, 0, 0)
     var max = Int.MinValue
 
-    for (z <- 2 to maxSize) {
-      for (x <- 1 to 301 - z; y <- 1 to 301 - z) {
-        val bottomLeft = dp(y + 1)(x)(z - 1)
-        val topRight = dp(y)(x + 1)(z - 1)
-        val center = dp(y + 1)(x + 1)(z - 2)
+    var dp_1 = grid.clone()
+    var dp_2 = Array.ofDim[Int](301, 301)
 
-        val power = bottomLeft + topRight - center + dp(y)(x)(1) + dp(y + z - 1)(x + z - 1)(1)
+    for (z <- 2 to maxSize) {
+
+      val next = Array.ofDim[Int](301, 301)
+
+      for (x <- 1 to 301 - z; y <- 1 to 301 - z) {
+        val bottomLeft = dp_1(y + 1)(x)
+        val topRight = dp_1(y)(x + 1)
+        val center = dp_2(y + 1)(x + 1)
+
+        val power = bottomLeft + topRight - center + grid(y)(x) + grid(y + z - 1)(x + z - 1)
 
         if(power > max) {
           max = power
           maxXyz = (x, y, z)
         }
 
-        dp(y)(x)(z) = power
+        next(y)(x) = power
       }
+
+      dp_2 = dp_1
+      dp_1 = next
     }
 
     maxXyz
