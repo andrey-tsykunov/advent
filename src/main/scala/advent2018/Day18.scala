90b5abc7f408ca0cf66bd92231bb18828155bc64
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2018-12-19 21:26:43 -0500

    Day18

---
+++ src/main/scala/advent2018/Day18.scala
@@ -0,0 +1,63 @@
+package advent2018
+
+import scala.annotation.tailrec
+
+
+object Day18 {
+
+  def calcResourceValue(map: Seq[String]): (Int, Int) =
+    (map.map(row => row.count(_ == '#')).sum, map.map(row => row.count(_ == '|')).sum)
+
+  def resourceValueAfterNMinutes(initial: Seq[String], n: Long): Int = {
+
+    @tailrec
+    def go(map: Seq[String], rvToTurn: Map[(Int, Int), Long], turnToRv: Map[Long, (Int, Int)], n: Long, cycleSize: Option[Int]): (Int, Int) = {
+      if(n == 0L || cycleSize.exists(n % _ == 0) ) return calcResourceValue(map)
+
+      def withinMap(row: Int, column: Int): Boolean = row >= 0 && row < map.size && column >= 0 && column < map(row).length
+
+      def nearby(row: Int, column: Int): Seq[Char] = {
+        for {
+          r <- row - 1 to row + 1
+          c <- column - 1 to column + 1 if withinMap(r, c) && (r != row || c != column)
+        }
+          yield map(r)(c)
+      }
+
+      def transform() = map.indices.map { r =>
+        map(r).indices.map { c =>
+          val grouped = nearby(r, c).groupBy(identity)
+
+          map(r)(c) match {
+            case '.' =>
+              if(grouped.getOrElse('|', Nil).size >= 3) '|'
+              else '.'
+            case '|' =>
+              if(grouped.getOrElse('#', Nil).size >= 3) '#'
+              else '|'
+            case '#' =>
+              if(grouped.getOrElse('#', Nil).nonEmpty && grouped.getOrElse('|', Nil).nonEmpty) '#'
+              else '.'
+          }
+        }.mkString
+      }
+
+      val r = transform()
+      val rv = calcResourceValue(r)
+
+      val size = rvToTurn.get(rv).flatMap { prev =>
+        val size = (prev - n).toInt
+
+        turnToRv.get(prev + size)
+          .filter(_ == rv)
+          .map(_ => size)
+      }
+
+      go(r, rvToTurn + (rv -> n), turnToRv + (n -> rv), n - 1, size)
+    }
+
+    val (lamber, wood) = go(initial, Map.empty, Map.empty, n, None)
+
+    lamber * wood
+  }
+}
---
+++ src/test/scala/advent2018/Day18Test.scala
@@ -0,0 +1,53 @@
+package advent2018
+
+import scala.io.Source
+
+class Day18Test extends UnitTest {
+
+  import Day18._
+
+  test("task 1") {
+    val data = readFrom("day18.txt")
+
+    resourceValueAfterNMinutes(data, 10) shouldBe 745008
+  }
+
+  test("task 1 (example)") {
+    val data = Seq(".#.#...|#.",
+      ".....#|##|",
+      ".|..|...#.",
+      "..|#.....#",
+      "#.#|||#|#|",
+      "...#.||...",
+      ".|....|...",
+      "||...#|.#|",
+      "|.||||..|.",
+      "...#.|..|.")
+
+    resourceValueAfterNMinutes(data, 10) shouldBe 1147
+//    Seq(
+//      ".......##.",
+//      "......|###",
+//      ".|..|...#.",
+//      "..|#||...#",
+//      "..##||.|#|",
+//      "...#||||..",
+//      "||...|||..",
+//      "|||||.||.|",
+//      "||||||||||",
+//      "....||..|."
+//    )
+  }
+
+  test("task 2") {
+    val data = readFrom("day18.txt")
+
+    // 1000000000L
+    resourceValueAfterNMinutes(data, 1000000000L) shouldBe 219425
+  }
+
+  private def readFrom(name: String): Seq[String] =
+    Source.fromResource(s"advent2018/$name")
+      .getLines()
+      .toStream
+}
