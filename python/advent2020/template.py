df0ff0670a7a03c844de26ba8e13bf53af65b5dc
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2020-11-30 23:00:32 -0500

    2020, setup

---
+++ python/advent2020/template.py
@@ -0,0 +1,38 @@
+
+from advent2019.utils import *
+
+input_file = "day1.txt"
+
+
+def task1(a):
+    return a
+
+
+def task2(a):
+    return a
+
+
+def test_task1():
+    a = []
+    r = task1(a)
+
+    assert r == 0
+
+
+def test_task2():
+    a = []
+    r = task2(a)
+
+    assert r == 0
+
+
+if __name__ == "__main__":
+    a = read_ints(input_file)
+
+    r1 = task1(a)
+    assert r1 == 0
+
+    r2 = task2(a)
+    assert r2 == 0
+
+
---
+++ python/advent2020/utils.py
@@ -0,0 +1,58 @@
+from os import path
+from decimal import Decimal
+import numpy as np
+import itertools
+
+dir_path = path.dirname(path.realpath(__file__))
+
+
+def read_strings(file_name):
+    file_path = path.join(dir_path, "data", file_name)
+    with open(file_path, "r") as f:
+        return f.read().splitlines()
+
+
+def read_strings_2d(file_name, split_by=","):
+    return [s.split(split_by) for s in read_strings(file_name)]
+
+
+def read_ints(name, split_by=","):
+    list2d = read_ints_2d(name, split_by)
+    return list(itertools.chain.from_iterable(list2d))
+
+
+def read_ints_2d(name, split_by=","):
+    return [[int(c) for c in s.split(split_by)] for s in read_strings(name)]
+
+
+def read_floats(name, split_by=","):
+    list2d = read_floats_2d(name, split_by)
+    return list(itertools.chain.from_iterable(list2d))
+
+
+def read_floats_2d(name, split_by=","):
+    return [[float(c) for c in s.split(split_by)] for s in read_strings(name)]
+
+
+def read_decimals(name):
+    return [Decimal(s) for s in read_strings(name)]
+
+
+def read_np_ints(name):
+    return np.array(read_ints(name), dtype=np.int)
+
+
+def read_np_int_2d(name, split_by=","):
+    a = [s.split(split_by) for s in read_strings(name)]
+    return np.array(a, dtype=np.int)
+
+
+def read_np_floats(name):
+    return np.array(read_floats(name), dtype=np.float32)
+
+
+def read_np_float_2d(name, split_by=","):
+    a = [s.split(split_by) for s in read_strings(name)]
+    return np.array(a, dtype=np.float32)
+
+
